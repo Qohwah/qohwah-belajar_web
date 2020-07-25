f64125e91fc468e40208a2987cb42b0b597065ad
Qohwah <sufyannurkhalim@gmail.com>
2020-07-26 00:01:25 +0700

    initialization commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Qohwah</title>
+    <link rel="stylesheet" href="fontawesome/css/all.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="qohwah-section">
+        <div class="inner-width">
+            <h1 class="section-title">qohwah 2020</h1>
+            <div class="border"></div>
+            <div class="qohwah-container">
+
+                <div class="qohwah-box">
+                    <div class="qohwah-icon">
+                        <i class="fas fa-users"></i>
+                    </div>
+                    <div class="qohwah-title">Database Santri</div>
+                    <div class="qohwah-desc"></div>
+                </div>
+
+                <div class="qohwah-box">
+                    <div class="qohwah-icon">
+                        <i class="fas fa-user-graduate"></i>
+                    </div>
+                    <div class="qohwah-title">Database Guru</div>
+                    <div class="qohwah-desc"></div>
+                </div>
+
+                <div class="qohwah-box">
+                    <div class="qohwah-icon">
+                        <i class="fas fa-envelope-open-text"></i>
+                    </div>
+                    <div class="qohwah-title">Surat-Surat</div>
+                    <div class="qohwah-desc"></div>
+                </div>
+
+                <div class="qohwah-box">
+                    <div class="qohwah-icon">
+                        <i class="fas fa-chart-line"></i>
+                    </div>
+                    <div class="qohwah-title">Laporan Bulanan</div>
+                    <div class="qohwah-desc"></div>
+                </div>
+
+                <div class="qohwah-box">
+                    <div class="qohwah-icon">
+                        <i class="fas fa-calendar-alt"></i>
+                    </div>
+                    <div class="qohwah-title">Jadwal Guru</div>
+                    <div class="qohwah-desc"></div>
+                </div>
+
+            </div>
+            
+            <div class="border2"></div>
+
+        </div>
+    </div>
+
+    <div class="footer">
+        <p>Copyright &copy; 2020 - <a href="">Qohwah</a></p>
+    </div>
+
+</body>
+</html>
